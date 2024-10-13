459c71601f7f2c0b54a3c2b79e43460700cd3d83
Pablo <59570362+Pekos123@users.noreply.github.com>
2024-10-13 16:27:16 +0200

    setup

---
+++ main.py
@@ -0,0 +1,31 @@
+# This code is based on the following example:
+# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
+
+import os
+import webserver
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+TOKEN = os.environ["TOKEN"]
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+
+webserver.keep_alive()
+
+client.run(TOKEN)
